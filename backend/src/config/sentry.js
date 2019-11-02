317fa8f693742d5eb4d54fa46d0248968abac11c
Angelo Brandão <angelobms@gmail.com>
2019-11-02 20:55:03 -0300

    commit initial

---
+++ backend/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  // dsn: 'https://50e2a90abb4e440abb80b315edabf49a@sentry.io/1797405',
+  dsn: process.env.SENTRY_DSN,
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+// import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionControler';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+/*
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Angelo Brandão',
+    email: 'angelobms@gmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
+});
+*/
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.post('/notifications', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;

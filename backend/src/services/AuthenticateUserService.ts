289dc8dc7bdf776eb1cdbb7d26073f4fa9001a80
Angelo Brandão <angelobms@gmail.com>
2020-08-14 09:27:39 -0300

    Added JWT token generation.

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { hash, compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
    user: User;
+   token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorret email/password combination.');
     }
 
+    const token = sign({}, '98891a60d85d0a6a036983fdf85042fb', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
     return {
       user,
+      token,
     }
   }
 }

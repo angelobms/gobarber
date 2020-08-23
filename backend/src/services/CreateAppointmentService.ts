8520db1566dc4fcc9f6aa5d1b6952a5945dcfd49
Angelo Brandão <angelobms@gmail.com>
2020-08-23 17:55:30 -0300

    Added class error.

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
@@ -19,7 +21,7 @@ class CreateAppointmentService {
     const findAppopintmentInSameDate = await appointmentRepository.findByDate(appointmentDate);
 
     if (findAppopintmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentRepository.create({
